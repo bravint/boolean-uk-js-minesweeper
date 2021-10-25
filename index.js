327e3235c67488bea52b89573c4e1449c121ddde
Bravin Thillainathan <bravint@gmail.com>
2021-10-25 18:15:12 +0100

    js: edited alert strings

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let numberMines = Number(prompt(`Select Difficulty: 1 for I'm Too Young to Die, 2 for Give me Plenty or 3 for Nightmare`));
 let n = 10;
+let lastArrayNumber = n - 1;
 let arr1 = Array.from(Array(n).keys())
 let decision = arr1.slice();
 let counter = 1;
@@ -10,13 +11,13 @@ arr1[rng] = 'mine';
 }
 
 for (let i = 0; i < arr1.length; i++) {
-    let position = Number(prompt(`choose a position to plant the flag`));
+    let position = Number(prompt(`choose a position to plant the flag from 0 to ${lastArrayNumber}`));
     decision[position] = 'mine';
     if (decision[position] == 'mine' && arr1[position] == 'mine') {
         console.log(`ggs you done goofed and hit a mine on turn ${counter}`)
         break
     } else if (counter == (n - numberMines)){
-        console.log(`ggs you have won`)      
+        console.log(`ggs you have won!`)      
         break
     } else {
         counter ++
