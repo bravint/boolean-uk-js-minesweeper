4df55bece16ad2c4cbb9ca1f0522b500c7a68124
Bravin Thillainathan <bravint@gmail.com>
2021-10-25 20:18:02 +0100

    js: improve readabilitty

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let numberMines = Number(prompt(`Select Difficulty: 1 for I'm Too Young to Die, 2 for Give me Plenty or 3 for Nightmare`));
 let n = 10;
 let lastArrayNumber = n - 1;
-let arr1 = Array.from(Array(n).keys())
+let arr1 = Array.from(Array(n).keys());
 let state = arr1.slice();
 let counter = 1;
 
@@ -14,12 +14,12 @@ for (let i = 0; i < arr1.length; i++) {
     let position = Number(prompt(`choose a position to plant the flag from 0 to ${lastArrayNumber}`));
     state[position] = 'mine';
     if (state[position] == arr1[position]) {
-        console.log(`gg you hit a mine on turn ${counter}`)
-        break
+        console.log(`gg you hit a mine on turn ${counter}`);
+        break;
     } else if (counter == (n - numberMines)){
-        console.log(`gg you have won!`)      
-        break
+        console.log(`gg you have won!`);     
+        break;
     } else {
-        counter ++
+        counter ++;
     }
 }
