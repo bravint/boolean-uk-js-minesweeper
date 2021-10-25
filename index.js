23b52bf107973ae625a17c2a4c0b097a9dea59b7
Bravin Thillainathan <bravint@gmail.com>
2021-10-25 18:03:28 +0100

    js: remove debugging console.log entries

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let numberMines = Number(prompt(`Select Difficulty: 1 for I'm Too Young to Die, 2 for Give me Plenty or 3 for Nightmare`));
 let n = 10;
 let arr1 = Array.from(Array(n).keys())
-console.log(arr1)
 let decision = arr1.slice();
 let counter = 1;
 
@@ -9,8 +8,6 @@ for (let i = 0; i <= numberMines; i++) {
 let rng = Math.floor(Math.random() * (10 - 1) + 1);
 arr1[rng] = 'mine';
 }
-
-console.log(arr1);
 
 for (let i = 0; i < arr1.length; i++) {
     let position = Number(prompt(`choose a position to plant the flag`));
