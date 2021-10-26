e544f070c952b3d343b12ba4b90cf2edea00449d
Bravin Thillainathan <bravint@gmail.com>
2021-10-26 09:13:39 +0100

    js: change player position input from mine to flag

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ for (let i = 0; i < numberMines; i++) {
 
 for (let i = 0; i < arr1.length; i++) {
     let position = Number(prompt(`choose a position to plant the flag from 0 to ${lastArrayNumber}`));
-        if (state[position] == 'mine') {
+        if (state[position] == 'flag') {
             console.log(`You have already placed a flag here, choose a another location`)
             i = i - 1;
         } else {
-            state[position] = 'mine';
-                if (state[position] == arr1[position]) {
+            state[position] = 'flag';
+                if (state[position] == 'flag' && arr1[position] == 'mine') {
                     console.log(`gg you hit a mine on turn ${counter}`);
                     break;
                 } else if (counter == (n - numberMines)){
